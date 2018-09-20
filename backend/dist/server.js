597ec9082bbf58085f72540151f6057150f8c4e6
Joni Korpelin <joni.korpelin@solita.fi>
2018-09-20 18:27:47 +0300

    removed dist

--- backend/dist/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-const port = 4040;
-app_1.default.listen(port, function () {
-    console.log('Express server listening on port ' + port);
-});
-//# sourceMappingURL=server.js.map
