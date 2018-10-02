a602a2d563be35fa6f558cacfeea89e125b28e4a
Joni Korpelin <joni.korpelin@solita.fi>
2018-10-02 12:21:14 +0300

    added sourcemap support

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,8 @@
 // tslint:disable-next-line:no-var-requires
 require('dotenv').config();
+// tslint:disable-next-line:no-var-requires
+require('source-map-support').install();
+
 import dotenv from 'dotenv';
 import app from './app';
 
